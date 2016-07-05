4b9169379d141d024fa790496532e486a35a4c85
nvidyakarthik <nvidya.karthik@gmail.com>
2016-07-05 01:04:59 -0700

    removed unnecessary dependencies for paid flavor

--- app/src/free/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
+++ app/src/free/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.jokedisplay.JokeActivity;
